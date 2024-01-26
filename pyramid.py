66335c27cc6bf3d044c9d2e23642b7c96874458e
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 21:39:00 +0800

    pseudocode

--- pyramid.py
+++ pyramid.py
@@ -1,1 +1,8 @@
 # Print a downward Half-Pyramid Pattern of Star (asterisk)
+
+# Pseudocode
+
+# Outer loop: Iterate from 6 to 1 in reverse order
+# Inner loop: Iterate 
+# Print '*' 
+# Move to the next line after inner loop
